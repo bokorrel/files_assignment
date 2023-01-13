2214baa51c5882ab0f93140865aa89ac69d3eff2
bokorrel <120737845+bokorrel@users.noreply.github.com>
2023-01-13 10:49:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 __winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
 __human_name__ = "files"
 
+import zipfile
+import os
+
+# create path
+current_directory = os.getcwd() #os.curdir
+#path = r"C:\Winc\files\cache"
+path = os.path.join(current_directory, 'cache')
+
 # clean_cache: takes no arguments and creates an empty folder named cache in the current directory. 
 # If it already exists, it deletes everything in the cache folder.
-def clean_cache():
-    import os
-    path = r"C:\Winc\files\cache"
+def clean_cache():   
     exist = os.path.exists(path)
     if exist != True:
         # create new folder
@@ -20,6 +26,7 @@ def clean_cache():
             # delete file
             os.remove(file_path)
             #print("file deleted")
+    return
 clean_cache()
 
 # cache_zip: takes a zip file path (str) and a cache dir path (str) as arguments, in that order. 
@@ -29,12 +36,11 @@ def cache_zip(zip_file_path,cache_dir_path):
     clean_cache()
     
     # get zip
-    import zipfile
     with zipfile.ZipFile(zip_file_path, 'r') as zObject:
         # extract files
         zObject.extractall(path=cache_dir_path)
 
-cache_zip(r"C:\Winc\files\data.zip",r"C:\Winc\files\cache")
+cache_zip(r"C:\Winc\files\data.zip",r"C:\Winc\cache")
 
 # cached_files: takes no arguments and returns a list of all the files in the cache. 
 # The file paths should be specified in absolute terms. 
@@ -42,9 +48,6 @@ cache_zip(r"C:\Winc\files\data.zip",r"C:\Winc\files\cache")
 # You do not have to account for files within folders within the cache directory.
 
 def cached_files():
-    import os
-    path = r"C:\Winc\files\cache"
-
     # create new list
     all_files = []
 
@@ -67,11 +70,8 @@ def find_password(file_paths):
         file = open(x)
         content = file.read()
         if content.find('password') != -1:
-            #print(x,":",content)
             part1 = content[content.find(':')+2:]
-            #print(part1)
             password = part1[:part1.find("\n")]
-            #print(password)
             return password
 
 print(find_password(cached_files()))
